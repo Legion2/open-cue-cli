d32731a1e234f297b830bd8db91c5d44366ea8b4
Leon Kiefer <leon.k97@gmx.de>
2020-04-27 18:56:57 +0200

    added active filter to profile list

--- src/main/kotlin/io/github/legion2/open_cue_cli/profile/ListProfiles.kt
+++ src/main/kotlin/io/github/legion2/open_cue_cli/profile/ListProfiles.kt
@@ -11,9 +11,12 @@ import kotlinx.coroutines.runBlocking
 
 class ListProfiles : CliktCommand("List all available profiles for the current game sorted by priority", name = "list") {
     private val details by option("-d", "--with-details", help = "Show the priority and state of all profiles").flag()
+    private val activeProfiles by option("-a", "--active", help = "Only list active profiles").flag()
     private val cliContext by requireObject<CliContext>()
     override fun run() = runBlocking {
-        val profiles = cliContext.sdkClient.listProfiles()
+        val profiles = cliContext.sdkClient.listProfiles().run {
+            if (activeProfiles) filter { profile -> profile.state } else this
+        }
         echo(profiles.sortedBy { it.priority }.echoString { profile -> if (details) profile.echoString else profile.name }, lineSeparator = "\n")
     }
 }
