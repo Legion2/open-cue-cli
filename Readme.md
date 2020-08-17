0179456ecfff4a9a0883f2f7b58b26554a7cb804
Leon Kiefer <leon.k97@gmx.de>
2020-08-17 20:09:30 +0200

    use gradle Badass Runtime plugin close #2

--- Readme.md
+++ Readme.md
@@ -6,7 +6,6 @@ You can use all your customized iCUE profiles.
 ## Getting Started
 Download and start the [Open CUE Service](https://github.com/Legion2/open-cue-service).
 Download the [latest release](https://github.com/Legion2/open-cue-cli/releases/latest) of Open CUE CLI and extract the archive.
-You must at least have Java 8 installed.
 Call the cli tool `open-cue-cli.bat` or `open-cue-cli` from a command prompt with the option `--help`.
 
 ```
@@ -67,9 +66,8 @@ You must set the name of the profile, when you export it from iCUE, you can't ch
 The priorities comes into play when you activate two profiles, then the profile with the higher priority is shown on top of the other.
 
 ## Packaging of this application
-This package is provide as zip containing executables and dependencies as jars.
-A Java Runtime Environment (JRE) must be installed to run the application.
-Just download the zip archive, extract it where you want and then execute one of the executable scripts from the extracted archive.
+This package is provide as zip containing optimized runtime images.
+Just download the [zip archive for your platform](https://github.com/Legion2/open-cue-cli/releases), extract it where you want and then execute one of the executable scripts from the extracted archive.
 
 ### Why not provide native executable
 Native executables require curl or other native implementations to do http calls.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
     application
+    id("org.beryx.runtime") version "1.11.2"
 }
 
 repositories {
@@ -14,11 +15,35 @@ application {
     applicationName = "open-cue-cli"
 }
 
+runtime {
+    addModules("java.base",
+            "java.sql",//because of gson
+            "jdk.unsupported"//https://stackoverflow.com/questions/61727613/unexpected-behaviour-from-gson
+    )
+
+    imageZip.set(file("$buildDir/${project.application.applicationName}.zip"))
+    targetPlatform("linux-x64") {
+        setJdkHome(jdkDownload("https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_x64_linux_hotspot_14.0.2_12.tar.gz"))
+    }
+    targetPlatform("linux-aarch64") {
+        setJdkHome(jdkDownload("https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_aarch64_linux_hotspot_14.0.2_12.tar.gz"))
+    }
+    targetPlatform("linux-arm32") {
+        setJdkHome(jdkDownload("https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_arm_linux_hotspot_14.0.2_12.tar.gz"))
+    }
+    targetPlatform("windows-x64") {
+        setJdkHome(jdkDownload("https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_x64_windows_hotspot_14.0.2_12.zip"))
+    }
+    targetPlatform("mac-x64") {
+        setJdkHome(jdkDownload("https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_x64_mac_hotspot_14.0.2_12.tar.gz"))
+    }
+}
+
 val ktorVersion = "1.3.2"
 val cliktVersion = "2.6.0"
 
 dependencies {
-    implementation("com.github.ajalt:clikt-multiplatform:$cliktVersion")
+    implementation("com.github.ajalt:clikt:$cliktVersion")
     implementation("io.ktor:ktor-client:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-json:$ktorVersion")
@@ -27,14 +52,22 @@ dependencies {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "1.8"
-        languageVersion = "1.3"
-        apiVersion = "1.3"
+        jvmTarget = "14"
+        languageVersion = "1.4"
+        apiVersion = "1.4"
         freeCompilerArgs += "-Xopt-in=com.github.ajalt.clikt.completion.ExperimentalCompletionCandidates"
     }
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.3"
+    gradleVersion = "6.6"
     distributionType = Wrapper.DistributionType.ALL
+}
+
+// workaround for https://github.com/beryx/badass-runtime-plugin/issues/67
+tasks.withType<CreateStartScripts> {
+    doLast {
+        windowsScript.writeText(windowsScript.readText()
+                .replace("set JAVA_HOME=\"%~dp0..\"", "set JAVA_HOME=\"%~dp0\""))
+    }
 }
