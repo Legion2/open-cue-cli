d79da470bb06837a3fffff597076335fb82696be
Leon Kiefer <leon.k97@gmx.de>
2022-07-17 19:50:56 +0200

    update ktor and kotlin version

--- src/main/kotlin/io/github/legion2/open_cue_cli/model/Profile.kt
+++ src/main/kotlin/io/github/legion2/open_cue_cli/model/Profile.kt
@@ -1,5 +1,8 @@
 package io.github.legion2.open_cue_cli.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Profile(val name: String, val priority: Int, val state: Boolean)
 
 val Profile.echoString: String get() = "$name $priority ${if (state) "active" else "not active"}"
